69a30283b8231c48f41f8e1533d65a258bf8ac55
ouki.wang <ouki.wang@dfrobot.com>
2018-06-22 09:57:17 +0800

    ds18b20

---
+++ README.md
@@ -0,0 +1,39 @@
+# DS18B20 Temperature Sensor for Microbit 
+
+## Table of Contents
+
+* [Summary](#summary)
+* [Blocks](#blocks)
+* [Example](#example)
+* [License](#license)
+
+## Summary
+DS18B20 module, support single bus control, and read temperature.
+
+## Blocks
+### Read temperature, return a number, makecode do not support floats, so the value is amplifies up to 100 times.
+![image](https://github.com/DFRobot/pxt-ds18b20/blob/master/image/number.png)<br>
+
+### Read temperature, return a string.
+![image](https://github.com/DFRobot/pxt-ds18b20/blob/master/image/string.png)<br>
+
+## Example
+Example for JavaScript
+```
+basic.forever(() => {
+    serial.writeValue("temp ", DS18B20.Temperature_number(DS18B20.pin.pin0))
+    basic.pause(1000)
+    serial.writeLine("temp : " + DS18B20.Temperature_string(DS18B20.pin.pin0))
+    basic.pause(1000)
+})
+```
+
+## License
+
+GUI
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+
---
+++ ds18b20.ts
@@ -0,0 +1,78 @@
+//% color=#5042f4 icon="\uf2c9"
+namespace DS18B20 {
+
+    // Functions for interfacing with the DS1820
+
+    /**
+     * Reads the temperature from the one-wire temperature sensor.
+     * Returns a 4 digit number. value should be divided by 100 to get 
+     *temperature in hundreths of a degree centigrade. 
+     * block="Temperature(C)"
+     */
+
+     export enum pin {
+       //% block=pin0
+       pin0 = 0,
+       //% block=pin1
+       pin1 = 1,
+       //% block=pin2
+       pin2 = 2,
+       //% block=pin5
+       pin5 = 5,
+       //% block=pin8
+       pin8 = 8,
+       //% block=pin11
+       pin11 = 11,
+       //% block=pin12
+       pin12 = 12,
+       //% block=pin13
+       pin13 = 13,
+       //% block=pin14
+       pin14 = 14,
+       //% block=pin15
+       pin15 = 15,
+       //% block=pin16
+       pin16 = 16
+     }
+     
+    //% shim=DS18B20::Temperature
+    export function Temperature(p: number): number {
+        // Fake function for simulator
+        return 0
+    }
+    
+    //% weight=10 blockId="Temperature_number" 
+    //% block="|%p| Temperature_number "
+    //% p.fieldEditor="gridpicker" p.fieldOptions.columns=4
+    export function Temperature_number(p: pin): number {
+        // Fake function for simulator
+        return Temperature(p)
+    }
+    
+    //% weight=10 blockId="Temperature_string" 
+    //% block="|%p| Temperature_string "
+    //% p.fieldEditor="gridpicker" p.fieldOptions.columns=4
+    export function Temperature_string(p: pin) : string{
+        let temp = Temperature(p);
+        let x = (temp / 100)
+        let y = (temp % 100)
+        let z = ''
+        if(temp >= 0){
+          if(y < 10){
+            z = x.toString() + '.0' + y.toString()
+          }
+          else{
+            z = x.toString() + '.' + y.toString()
+          }
+        }
+        else if(temp < 0){
+          if(y > -10){
+            z = '-' + (-x).toString() + '.0' + (-y).toString()
+          }
+          else{
+            z = '-' + (-x).toString() + '.' + (-y).toString()
+          }
+        }
+        return z
+    }
+}
